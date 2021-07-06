e94aaf9140f45f25f72d3fd48a71b200fc98c24a
Sumit <sj18.ec@gmail.com>
2021-07-06 22:56:20 +0530

    Image Picker for profile Image done

--- Messanger/Controllers/ConversationsViewController.swift
+++ Messanger/Controllers/ConversationsViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class ConversationsViewController: UIViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        // UserDefaults are used to save data about anything you want to save to disk. 
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+            
+        }
+        
     }
 
 
--- Messanger/Resources/Extensions.swift
+++ Messanger/Resources/Extensions.swift
@@ -4,5 +4,26 @@
 //
 //  Created by Sumit Joshi on 06/07/21.
 //
-
+import UIKit
 import Foundation
+
+extension UIView {
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+}
